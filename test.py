e5d20783f0fca01b7b4f5f6b6d19b182828c09c7
XXXG00W0 <ziyil0323@gmail.com>
2023-12-13 01:50:36 -0500

    More work done

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
-import tensorflow as tf
-base_model = tf.keras.applications.MobileNetV3Small(
-    input_shape=(224,224,3),
-    pooling='avg'
-)
-print(base_model.summary())
+# import tensorflow as tf
+# base_model = tf.keras.applications.MobileNetV3Small(
+#     input_shape=(224,224,3),
+#     pooling='avg'
+# )
+# print(base_model.summary())
+
+import PIL.Image as Image
+from PIL import ImageEnhance
+img = Image.open("custom_image_meat.jpg")
+enhancer = ImageEnhance.Brightness(img)
+factor_1 = 0.5
+factor_2 = 1.5
+shifted_image_1 = enhancer.enhance(factor_1)
+shifted_image_2 = enhancer.enhance(factor_2)
+shifted_image_1.show()
+shifted_image_2.show()
